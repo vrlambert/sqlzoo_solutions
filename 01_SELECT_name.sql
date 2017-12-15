0b6f518966eeedb860712afa045603ffaba3ffab
Victor Lambert <vrlambert@gmail.com>
2017-12-14 16:53:11 -0800

    SQLzoo SELECT name, 11-15 completed
    These are the harder questions with more complex filtering

--- 01_SELECT_name.sql
+++ 01_SELECT_name.sql
@@ -43,4 +43,32 @@ SELECT name FROM world
 SELECT name FROM world
  WHERE name LIKE '____';
 
- 
+-- 11 Find the country where the name is the capital city.
+SELECT name FROM world
+ WHERE name = capital;
+
+-- 12 Find the country where the capital is the country plus "City".
+SELECT name FROM world
+ WHERE capital = concat(name, ' City');
+
+-- 13 Find the capital and the name where the capital includes
+-- the name of the country.
+SELECT capital, name FROM world
+ WHERE capital LIKE concat(name, '%');
+
+/* 14 Find the capital and the name where the capital
+is an extension of name of the country.
+
+You should include Mexico City as it is longer than Mexico.
+You should not include Luxembourg as the capital is the same as the country. */
+SELECT capital, name FROM world
+ WHERE capital LIKE concat(name, '%')
+ AND LENGTH(capital) > LENGTH(name);
+
+/*For Monaco-Ville the name is Monaco and the extension is -Ville.
+
+Show the name and the extension where the capital is an
+extension of name of the country. */
+SELECT name, REPLACE(capital, name, '') FROM world
+ WHERE capital LIKE concat(name, '%')
+ AND LENGTH(capital) > LENGTH(name);
