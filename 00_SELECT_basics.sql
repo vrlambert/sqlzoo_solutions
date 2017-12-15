0f912eda53f8fdb8fdcf8416f601517a73f16883
Victor Lambert <vrlambert@gmail.com>
2017-12-14 16:23:43 -0800

    00 SELECT basics, queries 1-3 completed

---
+++ 00_SELECT_basics.sql
@@ -0,0 +1,15 @@
+/*
+The start of the sqlzoo tutorial, with the basic first select statements
+*/
+
+-- 1
+SELECT population FROM world
+  WHERE name = 'Germany';
+
+-- 2
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+-- 3
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000;
